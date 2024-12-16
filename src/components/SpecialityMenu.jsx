47e903028753c40d9381b2b573373be9acbaec4e
naeema-19 <naeemaziyad9@gmail.com>
2024-12-16 20:33:27 +0400

    initial

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
     <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-[#262626]'>
-      <h1 className='text-3xl font-medium'>Find by Speciality</h1>
-      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+      <h1 className='text-3xl font-medium'>Find by Services</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Take a look at our services</p>
       <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
         {
           specialityData.map((item, index) => (
