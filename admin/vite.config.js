704f70c670ecc6ebb1f25b5badda980af166c496
naeema-19 <naeemaziyad9@gmail.com>
2025-01-20 16:20:18 +0400

    initial

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
+  server:{server: {
+    host: '0.0.0.0',  // Allow external access
+    port: 5173        // Use a fixed port
+}}
 })
